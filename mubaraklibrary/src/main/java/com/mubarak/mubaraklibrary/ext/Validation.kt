9625b0b3364c72b98b4380ff205a2744665a8252
Mubarak <143716857+mubarakbacancy@users.noreply.github.com>
2023-12-18 12:10:46 +0530

    Initial commit

---
+++ mubaraklibrary/src/main/java/com/mubarak/mubaraklibrary/ext/Validation.kt
@@ -0,0 +1,19 @@
+package com.mubarak.mubaraklibrary.ext
+
+import android.content.Context
+import android.text.TextUtils
+import android.util.Patterns
+
+
+/**
+ * Created by :: Mubarak Ansari
+ * method is used for checking valid email id format.
+ *
+ * @param email email address as String
+ * @return boolean true for valid false for invalid
+ * Example : mubarakansari@gmail.com
+ */
+fun Context.isEmailValid(email: String): Boolean {
+    return !TextUtils.isEmpty(email.trim()) && Patterns.EMAIL_ADDRESS.matcher(email.trim())
+        .matches()
+}
